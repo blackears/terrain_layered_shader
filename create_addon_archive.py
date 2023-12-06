d823f4e0738245de1ef2fab0a91cbe0a60d5184c
Mark McKay <mark@kitfox.com>
2023-12-06 17:00:54 -0500

    Renaming files.  Creating python script to package up addon.

---
+++ create_addon_archive.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+# MIT License
+#
+# Copyright (c) 2023 Mark McKay
+# This file is for packaging Godot addons for various projects (https://github.com/blackears).
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+import shutil
+import sys
+import getopt
+import platform
+
+projectName = 'terrain_layered_shader'
+extensions = [".gd", ".tres", ".tscn", ".gdshaderinc", ".glsl", ".cfg", ".glb", ".gltf", ".jpg", ".jpeg", ".png", ".exr", ".bin"]
+
+# def copytree(src, dst):
+    # for item in os.listdir(src):
+    
+        # s = os.path.join(src, item)
+        # d = os.path.join(dst, item)
+        # if os.path.isdir(s):
+            # os.mkdir(d)
+            # copytree(s, d)
+        # else:
+            # filename, extn = os.path.splitext(item)
+            # #print ("file " + filename + " extn  " + extn)
+            # if not(extn in extensions):
+                # continue
+                
+            # shutil.copy(s, d)
+
+def copy_files_with_suffix(source_dir, dest_dir, suffixes):
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            if any(file.endswith(suffix) for suffix in suffixes):
+                source_path = os.path.join(root, file)
+                dest_path = os.path.join(dest_dir, os.path.relpath(source_path, source_dir))
+                os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+                shutil.copy2(source_path, dest_path)
+
+def make():
+    
+    #Create build directory
+    # curPath = os.getcwd()
+    if os.path.exists('build'):
+        shutil.rmtree('build')
+    # os.mkdir('build')
+    # os.mkdir('build/addons')
+    # os.mkdir('build/' + projectName)
+
+    copy_files_with_suffix("godot/addons/" + projectName + "/", "build/addons/" + projectName + "/", extensions);
+
+    
+    #Build addon zip file
+    if not os.path.exists('export'):
+        #shutil.rmtree('export')
+        os.mkdir('export')
+
+    shutil.make_archive("export/" + projectName, "zip", "build")
+
+
+
+if __name__ == '__main__':
+
+    make()
+
+            
+
